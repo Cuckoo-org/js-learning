b7627a935710522902a35d1034ae0b589e3427b3
hongfei <941116197@qq.com>
2018-11-10 21:26:17 +0800

    其它一些上传

---
+++ house/house.js
@@ -0,0 +1,10 @@
+// var oTab = document.getElementsByClassName("tab");
+// var oLists = oTab.getElementsByClassName("oList");
+// for(var i =1;i<oLists.length;i++){
+//     oLists[i].onmouseover = function(){
+//         this.style.backgroundColor = "pink";
+//     }
+//     oLists[i].onmouseout = function(){
+//         this.style.backgroundColor = "";
+//     }
+// }
